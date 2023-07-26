b5b5493eec6f635605f3ffb0cc1189a0124a9e2e
Krok <kadetkroksapsp2@gmail.com>
2023-07-26 21:57:38 +0200

    Initial commit

---
+++ app/src/main/java/com/todocomposeapp/data/ToDoDao.kt
@@ -0,0 +1,34 @@
+package com.todocomposeapp.data
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import com.todocomposeapp.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTask(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table WHERE id=:taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    @Query("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
+}
---
+++ app/src/main/java/com/todocomposeapp/data/models/Priority.kt
@@ -0,0 +1,14 @@
+package com.todocomposeapp.data.models
+
+import androidx.compose.ui.graphics.Color
+import com.todocomposeapp.ui.theme.HighPriorityColor
+import com.todocomposeapp.ui.theme.LowPriorityColor
+import com.todocomposeapp.ui.theme.MediumPriorityColor
+import com.todocomposeapp.ui.theme.NonePriorityColor
+
+enum class Priority (val color: Color) {
+    HIGH(HighPriorityColor),
+    LOW(LowPriorityColor),
+    MEDIUM(MediumPriorityColor),
+    NONE(NonePriorityColor)
+}
